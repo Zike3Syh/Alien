8e4258a0aeff8c3f80d7f12cee4e296dc10121f2
Zike3Syh <zike3booshi@163.com>
2018-05-21 16:06:15 +0800

    Finished

--- game_functions.py
+++ game_functions.py
@@ -63,7 +63,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets ):
+def check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets ):
     '''响应按键和鼠标事件'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -75,9 +75,9 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets 
             check_keyup_events(event, ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets,mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets,mouse_x, mouse_y):
     '''在玩家单击Play按钮时开始新游戏'''
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
@@ -88,6 +88,12 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         #重置游戏统计信息
         stats.reset_stats()
         stats.game_active = True
+
+        #重置记分牌图像
+        sb.prep_score()
+        sb.prep_high_score()
+        sb.prep_level()
+        sb.prep_ships()
 
         #清空外星人列表和子弹列表
         aliens.empty()
@@ -133,13 +139,18 @@ def check_bullet_alien_coolisions(ai_settings, screen, stats, sb, ship, aliens, 
     collisons = pygame.sprite.groupcollide(bullets, aliens, True, True)
     if collisons:
         for aliens in collisons.values():
-            stats.score += ai_settings.alien_points
+            stats.score += ai_settings.alien_points * len(aliens)
             sb.prep_score()
+        check_high_score(stats, sb)
 
     if len(aliens)  == 0:
         #删除现有子弹并新建外星人
         bullets.empty()
         ai_settings.increase_speed()
+        #提高等级
+        stats.level += 1
+        sb.prep_level()
+
         create_fleet(ai_settings, screen, ship, aliens)
 
 def check_fleet_edges(ai_settings, aliens):
@@ -155,12 +166,15 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
-    '''相应被外星人被撞倒的飞船'''
+def ship_hit(ai_settings, stats, sb, screen, ship, aliens, bullets):
+    '''响应被外星人被撞倒的飞船'''
     if stats.ships_left > 0:
 
         #将ship_left减1
         stats.ships_left -= 1
+
+        #更新记分牌
+        sb.prep_ships()
 
         #清空外星人列表和子弹列表
         aliens.empty()
@@ -176,16 +190,16 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         stats.game_active = False
         pygame.mouse.set_visible(True)
 
-def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+def check_aliens_bottom(ai_settings, stats, sb, screen, ship, aliens, bullets):
     '''检查是否有外星人到达底端'''
     screen_rect = screen.get_rect()
     for alien in aliens.sprites():
         if alien.rect.bottom >= screen_rect.bottom:
             #像飞船被撞倒一样处理
-            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            ship_hit(ai_settings, stats, sb, screen, ship, aliens, bullets)
             break
 
-def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+def update_aliens(ai_settings, stats, sb, screen, ship, aliens, bullets):
     '''检查是否有外星人位于屏幕边缘，
     并更新外形人群中所有外星人的位置'''
     check_fleet_edges(ai_settings, aliens)
@@ -193,7 +207,13 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
 
     #检测外星人和飞船之间的碰撞
     if pygame.sprite.spritecollideany(ship, aliens):
-        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+        ship_hit(ai_settings, stats, screen, sb, ship, aliens, bullets)
 
     #检查是否有外星人到达底端
-    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+    check_aliens_bottom(ai_settings, stats, sb, screen, ship, aliens, bullets)
+
+def check_high_score(stats, sb):
+    '''检查是否产生新的最高分'''
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,7 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 class ScoreBoard():
     '''显示得分信息的类'''
@@ -16,6 +19,21 @@ class ScoreBoard():
 
         #准备初始得分图像
         self.prep_score()
+        #准备最高得分图像
+        self.prep_high_score()
+        #显示等级
+        self.prep_level()
+        #显示剩余飞船
+        self.prep_ships()
+
+    def prep_ships(self):
+        '''显示还有多少剩余的飞船'''
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_settings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
 
     def prep_score(self):
         '''将得分转化为一幅渲染的图像'''
@@ -28,6 +46,29 @@ class ScoreBoard():
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        '''将最高分转换为渲染的图像'''
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.ai_settings.bg_color)
+
+        #将最高分放在屏幕中央
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def prep_level(self):
+        '''将等级转换为渲染的图像'''
+        self.level_image = self.font.render(str(self.stats.level), True, self.text_color, self.ai_settings.bg_color)
+
+        #将等级放在最左边
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
     def show_score(self):
-        '''在屏幕上显示得分'''
+        '''在屏幕上显示得分和最高分'''
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image,self.level_rect)
+        self.ships.draw(self.screen)
